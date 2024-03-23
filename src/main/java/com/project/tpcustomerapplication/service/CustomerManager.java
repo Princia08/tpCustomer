c6c6d7b2f42fcb8fca7ff265bf47f068a35f5c87
Princia08 <princiaram38@yahoo.com>
2024-03-23 14:25:45 +0300

    Ajout CustomerManager

---
+++ src/main/java/com/project/tpcustomerapplication/service/CustomerManager.java
@@ -0,0 +1,48 @@
+package com.project.tpcustomerapplication.service;
+
+import com.project.tpcustomerapplication.entity.Customer;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
+
+/**
+ * Gestionnaire des clients.
+ * Ce service gère les opérations liées aux clients tels que la création, la récupération et la mise à jour.
+ */
+@RequestScoped
+public class CustomerManager {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * Persiste un client dans la base de données.
+     * @param customer Le client à persister.
+     */
+    @Transactional
+    public void persist(Customer customer) {
+        em.persist(customer);
+    }
+
+    /**
+     * Récupère tous les clients de la base de données.
+     * @return La liste de tous les clients.
+     */
+    public List<Customer> getAllCustomers() {
+        Query query = em.createNamedQuery("Customer.findAll");
+        return query.getResultList();
+    }
+
+    /**
+     * Met à jour les informations d'un client dans la base de données.
+     * @param customer Le client à mettre à jour.
+     * @return Le client mis à jour.
+     */
+    @Transactional
+    public Customer update(Customer customer) {
+       return em.merge(customer);
+    }
+}
