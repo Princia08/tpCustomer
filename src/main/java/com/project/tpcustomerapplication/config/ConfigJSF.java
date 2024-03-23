e76b3c553524af1190394a949f141e11f36c8913
Princia08 <princiaram38@yahoo.com>
2024-03-23 17:56:56 +0300

    Création d'un bean géré par CDI

---
+++ src/main/java/com/project/tpcustomerapplication/config/ConfigJSF.java
@@ -0,0 +1,15 @@
+package com.project.tpcustomerapplication.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+
+/**
+ * Configuration JSF
+ * @author xxxx
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
